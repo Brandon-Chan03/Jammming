ce21d21b5f22c3ca21e7cac94f2ed5ae514daf36
Brandon-chan03 <Brandon.chan0216@gmail.com>
2022-04-19 00:59:06 -0500

    finished step 83

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,11 +1,30 @@
-let userToken;
+const clientId = '788bac81f5964bdaa3147c3e1a5e7af1'
+const redirectUri = "http://localhost:3000/"
+
+let accessToken;
 
 let Spotify = {
     getAccessToken() {
         if(userToken) {
             return userToken;
         }
+
+        const userTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/) 
+
+        if (userTokenMatch && expiresInMatch) {
+            accessToken = userTokenMatch[1]
+            const expiresIn = Number(expiresInMatch[1])
+            window.setTimeout(() => accessToken = '', expiresIn * 1000)
+            window.history.pushState('Access Token', null, '/')
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&
+            response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
+            window.location = accessUrl
+        }
     }
+
 }
 
 export default Spotify;
